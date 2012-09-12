65fb2b9f612fdd3759e41becb4952a21a9572459
Andreas Schempp <andreas.schempp@terminal42.ch>
2012-09-12 20:46:21 +0200

    Updated german language files

--- languages/de/default.php
+++ languages/de/default.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -31,21 +31,21 @@
 /**
  * Errors
  */
+$GLOBALS['TL_LANG']['ERR']['pwrecoveryFailed']	= 'Kein Benutzer mit diesem Namen und E-Mail-Adresse gefunden.';
+
 if (TL_MODE == 'BE')
 {
-	$GLOBALS['TL_LANG']['ERR']['invalidLogin']		= 'Anmeldung fehlgeschlagen! <a href="contao/pwrecovery.php?username=' . $this->Input->post('username') . '">Passwort vergessen?</a>';
-	$GLOBALS['TL_LANG']['ERR']['accountLocked']		= 'Das Konto wurde gesperrt! Sie können sich in %d Minuten erneut anmelden. <a href="contao/pwrecovery.php?username=' . $this->Input->post('username') . '">Passwort vergessen?</a>';
+	$GLOBALS['TL_LANG']['ERR']['invalidLogin']		= 'Anmeldung fehlgeschlagen! <a href="contao/index.php?pwrecovery=1">Passwort vergessen?</a>';
+	$GLOBALS['TL_LANG']['ERR']['accountLocked']		= 'Das Konto wurde gesperrt! Sie können sich in %d Minuten erneut anmelden. <a href="contao/index.php?pwrecovery=1">Passwort vergessen?</a>';
 }
-$GLOBALS['TL_LANG']['ERR']['pwrecoveryFailed']	= 'Kein Benutzer mit diesem Namen und E-Mail-Adresse gefunden.';
 
 
 /**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['MSC']['pwrecovery']		= 'Passwort zurücksetzen';
-$GLOBALS['TL_LANG']['MSC']['recoverBT']			= 'Neues Passwort senden';
-$GLOBALS['TL_LANG']['MSC']['pwrecoverySuccess']	= 'Es wurde Ihnen ein neues Passwort per E-Mail zugesendet. Bitte melden Sie sich mit dem neuen Passwort an.';
-$GLOBALS['TL_LANG']['MSC']['pwrecoverySubject']	= 'Neues Passwort für %s';
-$GLOBALS['TL_LANG']['MSC']['pwrecoveryMessage']	= "Auf %s wurde Ihr Passwort für den Benutzer %s zurückgesetzt.\n\nDas neue Passwort lautet: %s";
 $GLOBALS['TL_LANG']['MSC']['beLogin']			= 'Contao Backend-Login';
+$GLOBALS['TL_LANG']['MSC']['pwrecovery']		= 'Passwort zurücksetzen';
+$GLOBALS['TL_LANG']['MSC']['recoverBT']			= 'Passwort zurücksetzen';
+$GLOBALS['TL_LANG']['MSC']['pwrecoverySuccess']	= 'Sie erhalten nun eine E-Mail mit Hinweisen um Ihr Passwort zu ändern.';
+$GLOBALS['TL_LANG']['MSC']['pwrecoveryText']	= array('Ihre Passwort-Anforderung auf %s', "Sie haben ein neues Passwort für %s angefordert.\n\nBitte klicken Sie %s um das neue Passwort festzulegen. Wenn Sie diese E-Mail nicht angefordert haben, kontaktieren Sie bitte den Administrator der Webseite.\n");
 
